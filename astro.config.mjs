05aac3ed45969016acfe8f495e9b0c3dfe82705a
Johandev <162395710+HanjoBulKing7@users.noreply.github.com>
2026-05-15 13:21:32 -0600

    feat: First commit Project initialized

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://HanjoBulKing7.github.io',
+  base: '/Balconer-a-Omar',  // Si no es un repo de usuario/organización
+});
