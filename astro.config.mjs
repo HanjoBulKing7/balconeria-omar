18ac170694586d2fdff81185ea7e02eabaff2064
Johandev <162395710+HanjoBulKing7@users.noreply.github.com>
2026-05-15 19:50:24 -0600

    fea: Added hero page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://HanjoBulKing7.github.io',
-  base: '/Balconer-a-Omar',  // Si no es un repo de usuario/organización
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [icon()]
 });
