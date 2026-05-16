99395509fff2d031eacd97f940ab9aa6b8441617
Johandev <162395710+HanjoBulKing7@users.noreply.github.com>
2026-05-15 20:22:09 -0600

    fix: Fixed the astro.config file for proper setting up of actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://HanjoBulKing7.github.io',
+  base: '/balconeria-omar',
   vite: {
     plugins: [tailwindcss()]
   },
